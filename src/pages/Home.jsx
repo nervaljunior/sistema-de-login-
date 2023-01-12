8127b05c73765885e59250cdf7468a7289779575
nerval junior <nerval.junior@discente.ufma.br>
2023-01-12 15:24:32 -0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+
+  return (
+    <div><h1>lista de presença</h1>
+    <input type="text" placeholder="digite o nome"/>
+    <button type="button">adicionar</button> </div>
+  )
+}
+
+export default Home
