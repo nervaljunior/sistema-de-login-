1699b70ff4fd7e9c5fd631f71449c0dbd796b847
nerval junior <nerval.junior@discente.ufma.br>
2023-01-12 17:45:35 -0300

    adição de componentização inicial

---
+++ src/components/card/index.jsx
@@ -0,0 +1,14 @@
+import './styles.css' ;
+
+export function Card(){
+    return(
+        <div className='card'>
+            <strong>
+                nerval
+            </strong>
+            <small>
+                10:15:24
+            </small>
+        </div>
+    )
+}
