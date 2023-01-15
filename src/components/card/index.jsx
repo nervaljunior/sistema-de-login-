21a4b67308c8bd7ddff1f405f6344b4f2a80257c
nerval junior <nerval.junior@discente.ufma.br>
2023-01-15 20:18:38 -0300

    implementando erro de senha

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,14 +1,23 @@
-import './styles.css' ;
+/* import './styles.css' ; */
+export function Login({email, password, text}){
+    const delay = (0.7 + Math.random()*2)*1000;
 
-export function Card(){
-    return(
-        <div className='card'>
-            <strong>
-                nerval
-            </strong>
+    return new Promise ((resolve, reject)=>{
+        setTimeout(function(){
+            if (password==='nervalzao' && !!email | !!text){
+                resolve();
+            } else{
+                reject({message:'email or password wrong.'});
+            }
+        },delay);
+    });
+
+    /* return(
+        <div className='login'>
+            <a href="./">create a count</a>
             <small>
                 10:15:24
             </small>
         </div>
-    )
+    ) */
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,47 +2,89 @@ import React, {useState} from 'react';
 
 import './styles.css';
 
-import { Card } from '../../components/card';
+import { Login } from '../../components/card';
 
 
 export function Home() {
+  /* const [login, setStudentetname] =useState(); */
+//aqui eu começo a fazer a pegar os valores e fazer a comparação na arquivo login 
+  const[email,setEmail]=useState('');
+  const[password,setPassword]=useState('');
+  const[text,setText]=useState('');
+  const[error,setError]=useState('');
 
-  
+  const handleEmail=(event)=>{
+    const {value} = event.target;
+    setEmail(value);
+  };
 
-  const [login, setStudentetname] =useState();
+  const handlePassword=(event)=>{
+    const {value} = event.target;
+    setPassword(value);
+  };
 
+  const handleText=(event)=>{
+    const {value} = event.target;
+    setText(value);
+  };
+
+  const handleSubmit=()=>{
+    console.log('submited');
+
+    let values = {email:email,password:password, text:text};
+    Login(values)
+    .then(()=>{})
+    .catch((error)=>{ 
+      console.log(error);
+      setError(error)
+    });
+  };
 
 
   return (
 
-    <div className= "container"><h1> sistema de login</h1>
-    <label htmlFor="email">e-mail:{ login }</label>
-    <input 
-    type="email"
-    name="email" 
-    id="email" 
-    placeholder='blablabla@gmail.com'
-    pattern='.+globex\.com' 
-    onChange={e=>setStudentetname(e.target.value)}/>
+    <div>
+      <div className= "container"><h1> sistema de login </h1>
+     {/*  <header>
+        <div >
+          <img className='ball' src="https://github.com/nervaljunior.png" alt="perfil" />
+        </div>
+      </header> */}
+      <label htmlFor={'email'}>e-mail:{/* { login } */}</label>
+      <input 
+      type={'email'}
+      id={'email'}
+      placeholder='blablabla@gmail.com'
+      pattern='.+globex\.com' 
+      value={email}
+      onChange={handleEmail}
+      /* onChange={e=>setStudentetname(e.target.value)} *//>
 
-    <label htmlFor="text"> usuário: </label>
-    <input 
-    type="text" 
-    name='text' 
-    id="text" 
-    placeholder="digite o nome"
-    />
+      <label htmlFor={'text'}> usuário: </label>
+      <input 
+      type={'text'}
+      id={'text'} 
+      placeholder="digite o nome"
+      value={text}
+      onChange={handleText}
+      />
 
-    <label htmlFor="password"> senha: </label>
-    <input 
-    type="password" 
-    name='password' 
-    id='password' 
-    placeholder='difite sua senha' />
+      <label htmlFor={'password'}> senha: </label>
+      <input 
+      type={'password'}
+      id={'password'} 
+      value={password}    
+      onChange={handlePassword}
 
-    <button type="button">adicionar</button>
+      placeholder='difite sua senha' />
 
+      <button disabled={email==='' || password.length < 6 } onClick= {handleSubmit} >adicionar</button>
+
+      </div>
+      
+      {error && <div className='errorMenssage'>{error.message}</div>}
     </div>
+
   )
 }
 
